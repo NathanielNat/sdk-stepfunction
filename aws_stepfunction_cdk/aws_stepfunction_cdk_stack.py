420054692a7d613884c0570aafbdf404cb90dbf8
NathanielNat <psalmnat@gmail.com>
2022-09-21 18:31:11 +0000

    initial commit

--- aws_stepfunction_cdk/aws_stepfunction_cdk_stack.py
+++ aws_stepfunction_cdk/aws_stepfunction_cdk_stack.py
@@ -5,7 +5,10 @@ from aws_cdk import (
     aws_iam as iam,
     aws_sqs as sqs,
     aws_sns as sns,
+    aws_stepfunctions as sfn,
+    aws_stepfunctions_tasks as tasks,
     aws_sns_subscriptions as subs,
+    aws_lambda as lambda_
 )
 
 
@@ -14,13 +17,43 @@ class AwsStepfunctionCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "AwsStepfunctionCdkQueue",
-            visibility_timeout=Duration.seconds(300),
+        submit_lambda: lambda_.Function
+        get_status_lambda: lambda_.Function
+
+
+        submit_job = tasks.LambdaInvoke(self, "Submit Job",
+            lambda_function=submit_lambda,
+            # Lambda's result is in the attribute `Payload`
+            output_path="$.Payload"
         )
 
-        topic = sns.Topic(
-            self, "AwsStepfunctionCdkTopic"
+        wait_x = sfn.Wait(self, "Wait X Seconds",
+            time=sfn.WaitTime.seconds_path("$.waitSeconds")
         )
 
-        topic.add_subscription(subs.SqsSubscription(queue))
+        get_status = tasks.LambdaInvoke(self, "Get Job Status",
+            lambda_function=get_status_lambda,
+            # Pass just the field named "guid" into the Lambda, put the
+            # Lambda's result in a field called "status" in the response
+            input_path="$.guid",
+            output_path="$.Payload"
+        )
+
+        job_failed = sfn.Fail(self, "Job Failed",
+            cause="AWS Batch Job Failed",
+            error="DescribeJob returned FAILED"
+        )
+
+        final_status = tasks.LambdaInvoke(self, "Get Final Job Status",
+            lambda_function=get_status_lambda,
+            # Use "guid" field as input
+            input_path="$.guid",
+            output_path="$.Payload"
+        )
+
+        definition = submit_job.next(wait_x).next(get_status).next(sfn.Choice(self, "Job Complete?").when(sfn.Condition.string_equals("$.status", "FAILED"), job_failed).when(sfn.Condition.string_equals("$.status", "SUCCEEDED"), final_status).otherwise(wait_x))
+
+        sfn.StateMachine(self, "StateMachine",
+            definition=definition,
+            timeout=Duration.minutes(5)
+        )
